a71ec0aeb4028a8ead15166e00c4a959f9cff1e6
LucasAssisCosme <assislucas0201@gmail.com>
2025-09-04 16:41:19 -0300

    dor

---
+++ extreno.js
@@ -0,0 +1,16 @@
+console.log(process.argv);
+
+var argumentos = process.argv.slice(2)
+
+console.log(argumentos);
+
+var esporteEscolhido = argumentos[0]
+console.log("Seu esporte preferido é: ", esporteEscolhido);
+
+var dor = process.argv.slice(2)
+console.log(dor);
+
+var cantor = argumentos[1]
+console.log("Seu cantor preferido é: ", cantor);
+
+
